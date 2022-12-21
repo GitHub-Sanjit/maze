5956c4184a9f56727a27b448bd9dc010f89b9f97
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-21 14:35:44 +0600

    18. Determining Movement Direction

--- index.js
+++ index.js
@@ -69,12 +69,30 @@ const stepThroughCell = (row, column) => {
 
   // Assemble randomly-ordered list of neighbors
   const neighbors = shuffle([
-    [row - 1, column],
-    [row, column + 1],
-    [row + 1, column],
-    [row, column - 1],
+    [row - 1, column, "up"],
+    [row, column + 1, "right"],
+    [row + 1, column, "down"],
+    [row, column - 1, "left"],
   ]);
-  console.log(neighbors);
+
+  // For each neighbor
+  for (let neighbor of neighbors) {
+    const [nextRow, nextColumn, direction] = neighbor;
+    // see if that neighbors is out of bounds
+    if (
+      nextRow < 0 ||
+      nextRow >= cells ||
+      nextColumn < 0 ||
+      nextColumn >= cells
+    ) {
+      continue;
+    }
+
+    // if we have visited that neighbor, continue to next neighbor
+    if (grid[nextRow][nextColumn]) {
+      continue;
+    }
+  }
 };
 
 stepThroughCell(startRow, startColumn);
