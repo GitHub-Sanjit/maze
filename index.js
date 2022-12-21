b2facc778c19e66c9a1248e9ac0bd3ae14eac583
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-21 22:41:36 +0600

    32. Stretching the Canvas

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 const cells = 4; //  means 3/3
-const width = 600;
-const height = 600;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 const unitLength = width / cells;
 
