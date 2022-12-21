ec52f21b6597c5e88c91ecd5dc441e8b444611aa
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-21 14:23:27 +0600

    17. Shuffling Neighbor Pairs

--- index.js
+++ index.js
@@ -29,6 +29,20 @@ World.add(world, walls);
 
 // Maze Generation
 
+const shuffle = (arr) => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+    counter--;
+
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+  return arr;
+};
+
 const grid = Array(cells)
   .fill(null)
   .map(() => Array(cells).fill(false));
@@ -54,13 +68,14 @@ const stepThroughCell = (row, column) => {
   grid[row][column] = true;
 
   // Assemble randomly-ordered list of neighbors
-  const neighbors = [
+  const neighbors = shuffle([
     [row - 1, column],
     [row, column + 1],
     [row + 1, column],
     [row, column - 1],
-  ];
+  ]);
+  console.log(neighbors);
 };
 
 stepThroughCell(startRow, startColumn);
-console.log(grid);
+// console.log(grid);
